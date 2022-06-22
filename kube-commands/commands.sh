631700e5d7ba45b3747a7159aea4ea8a01249b64
iamtejasmane <tejasmane485@gmail.com>
2022-06-22 14:28:06 +0530

    commands updated

--- kube-commands/commands.sh
+++ kube-commands/commands.sh
@@ -37,3 +37,40 @@ kubectl apply -f <file>
 
 # to edit the pod configuration 
 kubectl edit pod <pod>
+
+# to create replicacontroller or replicaset from definition file
+kubectl create -f <definition-file-yml>
+
+# get replicaset
+kubectl get replicaset
+
+# get replicacontroller
+kubectl get replicacontroller
+
+# to replace or update the existing definition file
+kubectl replace -f <definition-file-yaml>
+ 
+# to scale replica using command line without modifying the file
+kubectl scale --replicas={count} <definition-file>
+kubectl scale replicaset <replicaset-name> --replicas={count}
+
+# delete replica set and also delete all underlaying PODs
+kubectl delete replicaset <replicaset-name>
+
+# descibe replicaset; all information
+kubectl describe replicaset <replicaset-name>
+
+# to edit existing configuration of the replicaset 
+# and open running configuration file to edit
+kubectl edit replicaset <replicaset-name>
+
+# to get info of all the cerated objects created in the cluster
+kubectl get all
+
+# to get deployments
+kubectl get deployments
+
+# to describe the deployments
+kubectl descibe deployment <deployment-name>
+
+
